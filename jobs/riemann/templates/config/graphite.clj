7672bfa285be855e1b6ac4ed8356f70486152086
Steve Harms <steven.harms@gsa.gov>
2016-02-29 09:57:31 -0500

    Update for new config

--- jobs/riemann/templates/config/graphite.clj
+++
@@ -1,4 +0,0 @@
-(defn add-environment-to-graphite [event] (str "production.hosts.", (riemann.graphite/graphite-path-percentiles event)))
-
-(def graph (async-queue! :graphite {:queue-size 1000}
-                         (graphite {:host "10.9.30.15" :path add-environment-to-graphite})))
