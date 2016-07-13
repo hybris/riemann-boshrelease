f484055ad78a7d6f52909a9bd851a121657f7ab4
Joshua Carp <jm.carp@gmail.com>
2016-07-12 21:00:36 -0400

    Fix metrics and simplify ctl script.

--- jobs/riemann-checklogs/templates/bin/check-logs.sh
+++ jobs/riemann-checklogs/templates/bin/check-logs.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
-set -e -x
+set -e -u -x
 
 TODAY=$(date +%Y.%m.%d)
 YESTERDAY=$(date -d "1 day ago" +%Y.%m.%d)
 
-TTL=${TTL:-600}
-JQ_PATH=${JQ_PATH:-/var/vcap/packages/jq-1.5/bin/jq}
-INTERVAL=${INTERVAL:-5m}
-ES_URL=${ES_URL:-localhost:9200}
+JQ_PATH=/var/vcap/packages/jq-1.5/bin/jq
 RIEMANNC_PATH=/var/vcap/jobs/riemannc/bin/riemannc
 
 function raw {
@@ -30,8 +27,7 @@ QUERY=$(${JQ_PATH} -n "{
 "})
 
 PLATFORM_LOGS=$(( $(query "platform" ${TODAY}) + $(query "platform" ${YESTERDAY}) ))
-${RIEMANNC_PATH} --service "logsearch.health.platform" --host $(hostname) --ttl ${TTL} --metric ${PLATFORM_LOGS}
+${RIEMANNC_PATH} --service "logsearch.health.platform" --host $(hostname) --ttl ${TTL} --metric_sint64 ${PLATFORM_LOGS}
 
 APP_LOGS=$(( $(query "app" ${TODAY}) + $(query "app" ${YESTERDAY}) ))
-${RIEMANNC_PATH} --service "logsearch.health.app" --host $(hostname) --ttl ${TTL} --metric ${APP_LOGS}
-
+${RIEMANNC_PATH} --service "logsearch.health.app" --host $(hostname) --ttl ${TTL} --metric_sint64 ${APP_LOGS}
