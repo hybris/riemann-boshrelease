e82767fda68823929cf5c4487662a6079cb6704d
Maxime Desrosiers <maxime.desrosiers@gmail.com>
2016-09-09 17:44:03 -0400

    fixing concourse update script

--- ci/bin/update_pipeline.sh
+++ ci/bin/update_pipeline.sh
@@ -4,12 +4,18 @@ set -e
 
 TARGET=${TARGET:-"hybris"}
 PIPELINE_NAME=${PIPELINE_NAME:-"riemann-boshrelease"}
-CREDENTIALS=$(mktemp /tmp/credentials.XXXXX)
 
 if ! [ -x "$(command -v fly)" ]; then
   echo 'fly is not installed.' >&2
 fi
 
+CREDENTIALS=$(mktemp /tmp/credentials.XXXXX)
+
 pull_credentials riemann concourse credentials.yml.erb ${CREDENTIALS}
 
-fly -t ${TARGET} set-pipeline -c pipeline.yml -l ${CREDENTIALS} -p ${PIPELINE_NAME}
+fly -t ${TARGET} set-pipeline -c pipeline.yml --load-vars-from=${CREDENTIALS} --pipeline=${PIPELINE_NAME}
+if [ $? -ne 0 ]; then
+  echo "Please login first: fly -t ${TARGET} login -k"
+fi
+
+rm -f ${CREDENTIALS}
